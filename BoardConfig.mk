b6a24bed5e5e58d4544cd6ebbd06ced1be934197
David Vandenburgh <davidvandenburgh@linux.com>
2016-11-30 16:22:18 -0500

    Revert "die permissive die"
    This reverts commit af6f2deb6d79f38a7ab89b1ca90515276760a64a.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -60,7 +60,7 @@ TARGET_USES_64_BIT_BINDER := true
 TARGET_CPU_CORTEX_A53 := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-3
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-3 androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100

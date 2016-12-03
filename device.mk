ad288c933b3383bc9c90d9f36e7d93d7aff1db1a
Dan Elkouby <streetwalrus@codewalr.us>
2016-12-03 21:04:06 +0200

    Nuke zram into oblivion
    Who the hell needs that on a phone with FOUR FUCKING GIGABYTES OF
    PHYSICAL MEMORY???

--- device.mk
+++ device.mk
@@ -217,10 +217,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     power.msm8994
 
-# Ramdisk
 PRODUCT_PACKAGES += \
-    init.qcom.bt.sh \
-    init.zram.sh
+    init.qcom.bt.sh
 
 PRODUCT_PACKAGES += \
     fstab.qcom \

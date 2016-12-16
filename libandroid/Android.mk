506fb1e53cd9de7e7a36f79ff4b5be36e6aa5e8c
Seraph08 <davidvandenburgh@linux.com>
2016-12-15 21:46:09 -0500

    proper libandroid

--- libandroid/Android.mk
+++ libandroid/Android.mk
@@ -1,5 +1,5 @@
 ifeq ($(TARGET_BUILD_PDK),true)
-ifeq ($(TARGET_BOARD_PLATFORM),msm8994)
+ifeq ($(TARGET_BOARD_PLATFORM),oneplus2)
 #----------------------------------------------------------------------
 # Fixup libandroid dependency
 #----------------------------------------------------------------------
@@ -14,7 +14,7 @@ LOCAL_SRC_FILES_arm64 := ../../../../$(PRODUCT_OUT)/obj/PACKAGING/pdk_fusion_int
 LOCAL_MULTILIB := both
 include $(BUILD_PREBUILT)
 
-$(LOCAL_PATH)/../../../../out/target/product/msm8994/obj/PACKAGING/pdk_fusion_intermediates/system/lib/libandroid.so : $(PRODUCT_OUT)/obj/PACKAGING/pdk_fusion_intermediates/pdk_fusion.stamp
-$(LOCAL_PATH)/../../../../out/target/product/msm8994/obj/PACKAGING/pdk_fusion_intermediates/system/lib64/libandroid.so : $(PRODUCT_OUT)/obj/PACKAGING/pdk_fusion_intermediates/pdk_fusion.stamp
+$(LOCAL_PATH)/../../../../out/target/product/oneplus2/obj/PACKAGING/pdk_fusion_intermediates/system/lib/libandroid.so : $(PRODUCT_OUT)/obj/PACKAGING/pdk_fusion_intermediates/pdk_fusion.stamp
+$(LOCAL_PATH)/../../../../out/target/product/oneplus2/obj/PACKAGING/pdk_fusion_intermediates/system/lib64/libandroid.so : $(PRODUCT_OUT)/obj/PACKAGING/pdk_fusion_intermediates/pdk_fusion.stamp
 endif
 endif

58a2cd8f2b53908defca4b464ea200eda68117d7
Seraph08 <davidvandenburgh@linux.com>
2017-01-13 16:12:13 -0500

    oneplus2: update power config kanged from AOSPA
    * improve performance by keeping one big core online
    * adjust corectl up/down values accordingly
    
    * adjust sched up/downmigrate to use the big core(s) more
    * use a sane interactive hispeed load value
    
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Conflicts:
    	rootdir/etc/init.qcom.post_boot.sh

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -113,9 +113,9 @@ case "$target" in
          echo 40 > /sys/module/cpu_boost/parameters/input_boost_ms
          # configure core_ctl module parameters
          echo 4 > /sys/devices/system/cpu/cpu4/core_ctl/max_cpus
-         echo 0 > /sys/devices/system/cpu/cpu4/core_ctl/min_cpus
+         echo 1 > /sys/devices/system/cpu/cpu4/core_ctl/min_cpus
          echo 70 > /sys/devices/system/cpu/cpu4/core_ctl/busy_up_thres
-         echo 10 > /sys/devices/system/cpu/cpu4/core_ctl/busy_down_thres
+         echo 30 > /sys/devices/system/cpu/cpu4/core_ctl/busy_down_thres
          echo 100 > /sys/devices/system/cpu/cpu4/core_ctl/offline_delay_ms
          echo 1 > /sys/devices/system/cpu/cpu4/core_ctl/is_big_cluster
          echo 4 > /sys/devices/system/cpu/cpu4/core_ctl/task_thres

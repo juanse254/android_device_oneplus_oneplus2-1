dd89850e0b6f9b9c9658ccb58c0193e63426848a
Seraph08 <davidvandenburgh@linux.com>
2017-02-24 00:18:53 -0500

    Revert "BoardConfig: enable DexPreopt for userdebug builds"
    This reverts commit 4cb197012d1578465a097700a2f2d8abe8dd5fb8.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -198,7 +198,7 @@ BOARD_HAL_STATIC_LIBRARIES := libhealthd.msm
 
 # Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
-  ifneq ($(findstring $(TARGET_BUILD_VARIANT),user userdebug),)
+  ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user),true)
     ifeq ($(WITH_DEXPREOPT),)
       WITH_DEXPREOPT := true
     endif
